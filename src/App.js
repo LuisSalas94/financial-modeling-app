47dc27333f50f7ce253707c4a13ae31cbf1c8524
Fernando <luisfernandosalasg@gmail.com>
2022-06-20 11:16:04 -0500

    Add Routing config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable */
+import "./App.css";
+//react router
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+//components
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import Details from "./components/details/Details";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/details/:idCompany" element={<Details />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
---
+++ src/components/details/Details.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+import React from "react";
+
+const Details = () => {
+	return <div>Details</div>;
+};
+
+export default Details;
