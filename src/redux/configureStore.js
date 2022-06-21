716b27672b156a29cc87ceff95a2ecc6529bfd90
Fernando <luisfernandosalasg@gmail.com>
2022-06-21 09:50:47 -0500

    Add Redux config for Details

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,8 +3,9 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 import homeReducer from "./home/home";
+import detailsReducer from "./details/details";
 
-const rootReducer = combineReducers({ homeReducer });
+const rootReducer = combineReducers({ homeReducer, detailsReducer });
 const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 export default store;
